6d11edd1c66b75648e2fa4d6fd97b0c4eb751fd2
Albermonte <14013679+Albermonte@users.noreply.github.com>
2023-07-28 11:49:11 +0200

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-export const one = 1
-export const two = 2
+import type { Client, PlainStakingContract, PlainTransactionDetails } from '@nimiq/core-web';
+
+let clientPromise: Promise<Client>;
+
+const getNetworkClient = async () => {
+    clientPromise = clientPromise || (async () => {
+        const { ClientConfiguration, Client } = await import('@nimiq/core-web');
+
+        const clientConfig = new ClientConfiguration();
+        clientConfig.network('testalbatross');
+        clientConfig.logLevel('debug');
+
+        return Client.create(clientConfig.build());
+    })();
+    return clientPromise;
+}
+
+
+const client = await getNetworkClient();
+
+client.addConsensusChangedListener((consensus) => {
+    console.log('Consensus changed:', consensus);
+    if (consensus === 'established') {
+        // We are connected to the network and fully synced
+        console.log('Consensus established!')
+    }
+});
